96ee83df1e4b63bb266dc68c1a5ff2a50b065e40
neel-stark <40642577+neel-stark@users.noreply.github.com>
2018-06-30 10:11:32 +0200

    main uptil procrustes
    also changed threshold in ransac

--- SOLN_Final/main.m
+++ SOLN_Final/main.m
@@ -1,14 +1,8 @@
-tag = 1;
 %Enter the image, hesaff and heraff directories here
-if tag == 0
 hesaff_dir = 'C:\D\SC coursework\Q3\computer vision\assignments\Final Project\modelCastle_features\hesaff';
 haraff_dir = 'C:\D\SC coursework\Q3\computer vision\assignments\Final Project\modelCastle_features\haraff';
 im_dir = 'C:\D\SC coursework\Q3\computer vision\assignments\Final Project\model_castle_png';
-elseif tag == 1  
-hesaff_dir = 'D:\TU Delft\Courses\Computer Vision\Assignments\modelCastle_features\hesaff';
-haraff_dir = 'D:\TU Delft\Courses\Computer Vision\Assignments\modelCastle_features\haraff';
-im_dir = 'D:\TU Delft\Courses\Computer Vision\Assignments\modelCastlePNG';    
-end
+
 file_name = @(dir)[dir.folder '\' dir.name];
 
 %% Reading the features and image files
@@ -38,42 +32,228 @@ for i = 1:n_imgs
 end
 toc
 
-%% Matching the features in subsequent images and Improving matches 
-%  using normalized 8 point RANSAC Algorithm
+%% selecting foreground section for each image:
+x = cell(1,length(im));
+y = cell(1,length(im));
+
+%Allowing user to select points to mark foreground boundary
+for i = 1: length(im)
+   imshow(im{i},[]);
+   [x{i},y{i}] = ginput();
+   x{i}(end+1) = x{i}(1);
+   y{i}(end+1) = y{i}(1);
+end
+
+%displaying the foreground selection on the images
+for j = 1 : length(im)
+   figure();
+   imshow(im{j},[]); hold on
+   plot(x{j},y{j});
+   pause(2);
+end
+
+%% Eliminating background features and descriptors
+for i = 1:length(feat)
+    in = inpolygon(feat{i}(1,:),feat{i}(2,:),x{i},y{i});
+    feat{i} = feat{i}(:,in);
+    desc{i} = desc{i}(:,in);
+end
+
+for j = 1 : length(im)
+   figure();
+   imshow(im{j},[]); hold on
+   plot(feat{j}(1,:),feat{j}(2,:),'*')
+   pause(2);
+end
+
+%% Matching the features in subsequent images Using vl_ubcmatch
+
 match_thresh = 2;               %tune the matching threshold here
 newmatches = cell(1,n_imgs);
+matches = cell(1,n_imgs);
 Ff = cell(1,n_imgs);
 
+%getting matches using vl_ubc match
 for i = 1:n_imgs
-    feat1 = [feat_haraff{i}, feat_hesaff{i}];
-    desc1 = [desc_haraff{i}, desc_hesaff{i}];
+    feat1 = feat{i};
+    desc1 = desc{i};
     
     if i == n_imgs
-        feat2 = [feat_haraff{1}, feat_hesaff{1}];
-        desc2 = [desc_haraff{1}, desc_hesaff{1}];
+        feat2 = feat{1};
+        desc2 = desc{1};
     else
-        feat2 = [feat_haraff{i+1}, feat_hesaff{i+1}];
-        desc2 = [desc_haraff{i+1}, desc_hesaff{i+1}];
+        feat2 = feat{i+1};
+        desc2 = desc{i+1};
     end
     
-    %Finding matches
-    [matches,~] = vl_ubcmatch(desc1,desc2);     %,match_thresh
+    %Finding initial matches using vl_ubcmatch
+    [matches{i},~] = vl_ubcmatch(desc1,desc2,match_thresh);     %,match_thresh
     
 %     %plotting matches
-%     plotmatches(im{1},im{2},feat1,feat2,matches,'Stacking','o');        
+%     plotmatches(im{1},im{2},feat1,feat2,matches{i},'Stacking','o');   
+end
+
+%% Refining matches using normalized 8 point RANSAC algorithm
+sampson_dist_mat = cell(1,n_imgs);
+for i = 1:n_imgs
+    feat1 = feat{i};
+    desc1 = desc{i};
+    
+    if i == n_imgs
+        feat2 = feat{1};
+        desc2 = desc{1};
+    else
+        feat2 = feat{i+1};
+        desc2 = desc{i+1};
+    end
     
     %getting coordinates of matched points
-    m1 = feat1(1:2,matches(1,:));
-    m2 = feat2(1:2,matches(2,:));
+    m1 = feat1(1:2,matches{i}(1,:));
+    m2 = feat2(1:2,matches{i}(2,:));
     
-    [Ff{i}, inliers_idx_star, ~] = norm_8p_algo_RANSAC(m1,m2);
+    %refining the matches using RANSAC
+    [Ff{i}, inliers_idx_star, ~,sampson_dist_mat{i}] = norm_8p_algo_RANSAC(m1,m2);
+    newmatches{i} = matches{i}(:,inliers_idx_star);
     
-    newmatches{i} = matches(:,inliers_idx_star);
-    
-    %plotmatches(im{1},im{2},feat1,feat2,newmatches{end},'Stacking','o');
+%     %plotting newmatches
+%     figure()
+%     if i==n_imgs
+%         plotmatches(im{i},im{1},feat1,feat2,newmatches{i},'Stacking','o');
+%     else
+%         plotmatches(im{i},im{i+1},feat1,feat2,newmatches{i},'Stacking','o');
+%     end
+%     pause(2);
 end
+
+%% checking RANSAC output for specific images:
+n = 16;
+figure()
+plotmatches(im{n},im{n+1},feat{n},feat{n+1},matches{n},'Stacking','o');
+figure()
+plotmatches(im{n},im{n+1},feat{n},feat{n+1},newmatches{n},'Stacking','o');
+
+
+%% Tuning threshold
+edges = logspace(0,6,60);
+N = cell(size(sampson_dist_mat));
+Ncum = cell(size(sampson_dist_mat));
+bin = cell(size(sampson_dist_mat));
+
+for j=1:n_imgs
+    for i = 1:size(sampson_dist_mat{j},1)
+        N{j}(i,:) = histcounts(abs(sampson_dist_mat{j}(i,:)),edges);
+        Ncum{j}(i,:) = cumsum(N{j}(i,:))/size(sampson_dist_mat{j},2)*100;
+        bin{j}(i) = find(Ncum{j}(i,:)>5,1);
+    end
+    bin_mean(j)=floor(mean(bin{j}));
+end
+
 
 %% Chaining
 point_view_matrix = chaining(newmatches);
 
+% %% Extracting columns with more than 2 matches in point_view_matrix
+% point_view_matrix_crop = point_view_matrix;
+% reps = zeros(1,size(point_view_matrix,2));
+% 
+% for i = 1:size(point_view_matrix,2)
+%     reps(i) = length(find(point_view_matrix(:,i)));
+% end
+% 
+% point_view_matrix_crop = point_view_matrix_crop(:,(reps>2));
+
+%%  Extracting blocks of features that have matches in 3 consecutive images
+
+%copying first two rows to the bottom of Point_view_matrix to complete the loop
+PVM = [point_view_matrix; point_view_matrix(1:2,:)]; 
+
+%Initializing Point_view_matrix blocks: PVMb
+PVMb = cell(1,size(point_view_matrix,1));
+%Initializing a cell array to store the column number of features in PVM
+%that are in a block
+column_no = cell(1,size(point_view_matrix,1));
+
+for i = 1:size(point_view_matrix,1)
+    for j = 1:size(PVM,2)
+        if length(find(PVM(i:i+2,j)))==3
+            PVMb{i} = [PVMb{i},PVM(i:i+2,j)];
+            column_no{i} = [column_no{i},j];
+        end
+    end
+end
+%% Solving for structure and Motion matrices for each block
+PVMb = PVMb(1:12);
+column_no = column_no(1:12);
+
+M_cell = cell(size(PVMb));
+S_cell = cell(size(PVMb));
+
+% appending features of image 1 and image 2 at the bottom of feat to
+% complete loop
+feat{20} = feat{1} ; feat{21} = feat{2};
+
+for i = 1:size(PVMb,2)
+    Points = zeros(6,size(PVMb{i},2));
+    for j =1:3
+        Points(j*2-1:j*2,:) = feat{i+j-1}(1:2,PVMb{i}(j,:));
+    end
+    PointsC = Points - mean(Points,2);
+    
+    %Singular Value Decomposition
+    [U,W,V] = svd(PointsC);
+    
+    U = U(:,1:3);
+    W = W(1:3,1:3);
+    V = V(:,1:3);
+    
+    M = U*sqrtm(W);
+    S = sqrtm(W)*V';
+    
+
+    M_cell{i} = M;
+    S_cell{i} = S;
+end
+    
+%% using Procustes
+for i = length(S_cell):-1:2
+    [~,ia,ib] = intersect(column_no{i-1},column_no{i});
+    [~,~,transform] = procrustes(S_cell{i-1}(:,ia)',S_cell{i}(:,ib)');
+    
+    %Points from next frame to be converted to current frame coordinate system
+    [~,~,ib] = setxor(column_no{i-1},column_no{i});
+    Y = S_cell{i}(:,ib);
+    
+    %transforming these points to the current coordinate frame
+    Z = transform.b*Y'*transform.T + repmat(transform.c(1,:),size(Y,2),1);
+    
+    %Appending these points to the current S matrix
+    S_cell{i-1} = [S_cell{i-1},Z'];
+    
+    %Appending new column number to the current frame
+    column_no{i-1} = [column_no{i-1},column_no{i}(ib)];
+end
+
+
+%%
+    save('M','M');
+    
+    %solve for affine ambiguity
+    L0= zeros(3);
+    %Solve for L
+    L = lsqnonlin(@myfun,L0);
+    % Recover C
+    C = chol(L,'lower');
+    % Update M and S
+    M = M*C;
+    S = pinv(C)*S;
+    
+
+
+% %%
+% feat = cell(1,length(feat_haraff));
+% desc = cell(1,length(feat_haraff));
+% for i = 1:length(feat_haraff)
+%     feat{i} = [feat_haraff{i}, feat_hesaff{i}];
+%     desc{i} = [desc_haraff{i}, desc_hesaff{i}];
+% end
 
--- SOLN_Final/norm_8p_algo_RANSAC.m
+++ SOLN_Final/norm_8p_algo_RANSAC.m
@@ -1,12 +1,15 @@
-function [Ff, inliers_idx_star, n_inliers_star] = norm_8p_algo_RANSAC(m1,m2)
-N = 350;             %No. of iterations
+function [Ff, inliers_idx_star, n_inliers_star,sampson_dist_mat] = norm_8p_algo_RANSAC(m1,m2)
+N = 500;             %No. of iterations 
+%For the first 150 iterations we tune the threshold, and for remaining 350
+%iterations we run RANSAC
+
 n_inliers_star = 0;  %max no. of inliers initialization
 
 %Normalizing the matched points
 [m1_hat , T1] = normalize_points(m1);
 [m2_hat , T2] = normalize_points(m2);
 
-dist_thresh = 1000;           %tune the inliers distance threshold here
+dist_thresh = 1800;           %tune the inliers distance threshold here
 
 for i = 1:N
     %Finding the model based on 8 sample points
@@ -42,8 +45,8 @@ for i = 1:N
     %sampson distance vector:
     sampson_dist = num'./den;
     
-%     %Uncomment for getting sampson_dist matrix to tune threshold
-%     sampson_dist_mat(i,:) = sampson_dist;
+    %Uncomment for getting sampson_dist matrix to tune threshold
+    sampson_dist_mat(i,:) = sampson_dist;
     
     %checking for inliers              
     inliers_bool = abs(sampson_dist) < dist_thresh;
@@ -61,7 +64,7 @@ end
 
 %Recomputing F with max number of inliers:
 A_hat = formulate_eq(m1_hat(:,inliers_idx_star),m2_hat(:,inliers_idx_star));
-[~,~,V] = svd(A_hat,'econ');
+[~,~,V] = svd(A_hat);
 
 f = V(:,9);          %the elements of fundamental matrix in  a 9x1 vector
 F_hat = reshape(f,[3,3]);
